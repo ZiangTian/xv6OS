0d7a89ed3c0c3bf2195a73fcb856a48d20641764
ziangtian <bienlor@163.com>
2024-10-03 01:40:50 +0800

    symlink done

--- kernel/fs.c
+++ kernel/fs.c
@@ -538,7 +538,7 @@ writei(struct inode *ip, int user_src, uint64 src, uint off, uint n)
     return -1;
 
   for(tot=0; tot<n; tot+=m, off+=m, src+=m){
-    bp = bread(ip->dev, bmap(ip, off/BSIZE));
+    bp = bread(ip->dev, bmap(ip, off/BSIZE));  // the bmap does the block allocation
     m = min(n - tot, BSIZE - off%BSIZE);
     if(either_copyin(bp->data + (off % BSIZE), user_src, src, m) == -1) {
       brelse(bp);
